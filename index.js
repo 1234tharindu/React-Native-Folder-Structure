ce04136b68bc6f3d4c023f6677fe595c4c098ab4
Tharindu <96545@fhss.sjp.ac.lk>
2023-09-12 09:44:22 +0530

    init commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ * This is the first change
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './app/Root';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
